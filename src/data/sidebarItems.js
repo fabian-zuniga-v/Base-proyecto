6b8eac90b879189a4e824fe1a8c55bc7ac7edf98
CENALOCAL\rmanques <rmanques@cenabast.cl>
2026-01-29 16:51:06 -0300

    Cambio de diseño a tailwind

--- src/data/sidebarItems.js
+++
@@ -1,7 +0,0 @@
-export const SIDEBAR_ITEMS = [
-  { type: 'item', label: 'Inicio', icon: '/iconHome.svg' },
-  { type: 'item', label: 'Panel', icon: '/iconPanel.svg' },
-  { type: 'dropdown', label: 'Gestion Administrativa', icon: '/iconSettings.svg', children: ['Usuarios','Contratos','Configuración'] },
-  { type: 'item', label: 'Reportes', icon: '/iconReportes.svg' },
-  { type: 'item', label: 'Manuales y Documentación', icon: '/iconManual.svg' }
-]
